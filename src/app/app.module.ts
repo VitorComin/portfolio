fee47538502a820e993139bc4a302a0782f33798
VitorComin <vitorgabrielcomin@gmail.com>
2023-09-05 14:52:23 -0300

    First Commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { ExperienceComponent } from './components/experience/experience.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    AboutComponent,
+    ExperienceComponent,
+    ProjectsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,32 @@
+import { Component, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent implements AfterViewInit {
+  @ViewChild('mobileMenu') mobileMenu: ElementRef | undefined;
+  @ViewChild('navList') navList: ElementRef | undefined;
+  @ViewChild('links') links: ElementRef | undefined;
+
+  constructor() {}
+
+  ngAfterViewInit() {
+    this.initMobileNavbar();
+  }
+
+  initMobileNavbar() {
+    if (this.mobileMenu && this.navList) {
+      this.mobileMenu.nativeElement.addEventListener('click', () => {
+        if (this.navList) {
+          this.navList.nativeElement.classList.toggle('active');
+        }
+        if (this.links) {
+          this.links.nativeElement.classList.toggle('active');
+          console.log("funciona")
+        }
+      });
+    }
+  }
+}
